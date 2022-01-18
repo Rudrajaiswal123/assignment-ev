b507b135d0452c891912415c5e742ddaeba01d06
rudra <you@example.com>
2022-01-18 19:01:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState({});
+
+   const getDataDetails = async () => {      
+    const apiURL ="https://randomuser.me/api";
+    await axios.get(apiURL)
+      .then((res) => {
+      console.log(res);
+      setData(res.data.results[0]);
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+  }
+
+  useEffect(() => {
+    getDataDetails();
+  }, []);
+
+  const handleRefresh = () => {
+    getDataDetails();
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">     
+      <div className="container w-25 mx-auto mt-25  bg-info p-2">          
+        <h3 className=" text-white">Full Name</h3>
+        <h5 className="text-white mb-4">{data?.name?.title} {data?.name?.first} {data?.name?.last}</h5>
+        <h3 className="text-white">Email Address</h3>
+        <h5 className="text-white mb-3">{data?.email}</h5>
+        <button type="button" className="btn btn-danger text-white  fw-bold" onClick={handleRefresh}>REFRESH</button>          
+      </div>      
     </div>
   );
 }
